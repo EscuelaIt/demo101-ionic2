ec4dc464e4a2d27a3d8a77545f7d2a9106376b80
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-02-27 13:48:14 -0500

    add class person

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Person } from '../../models/person.model';
+
 
 @Component({
   selector: 'page-home',
@@ -9,7 +11,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    
+    let person = new Person('nicolas',23);
+    let name = person.getName();
+    console.log(name);
   }
 
 }
