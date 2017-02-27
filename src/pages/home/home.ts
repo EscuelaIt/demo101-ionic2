23adb47a44ff2d2c192466de66a075fa5dcc9cbe
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-02-27 13:29:53 -0500

    delete models

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import { Person } from '../../models/person.model';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,9 +9,7 @@ import { Person } from '../../models/person.model';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-    let person1 = new Person('nicolas', 23);
-    let name = person1.getName();
-    console.log(name);
+    
   }
 
 }
